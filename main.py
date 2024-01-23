c76f10b4b312c9b85d93376cc5ea586b4134ebb2
oldmcdonnell <oldmcdonnell@gmail.com>
2024-01-22 22:41:10 -0500

    start video and end sequence

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import cv2
+import time
 
 video = cv2.VideoCapture(0)
-check, frame = video.read()
+time.sleep(1) # wait for video to process
 
-print(check)
-print(frame)
+while True:
+    check, frame = video.read() # reads vide
+    cv2.imshow("My Video", frame) # sends data to visual file
+
+    key = cv2.waitKey(1) #keyboard key object
+
+    if key == ord("q"): #break if use presses q
+        break
+
+video.release()
