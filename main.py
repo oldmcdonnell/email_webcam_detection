9a98fac6b06502eb5bc083d4c9d2627ef8611d31
oldmcdonnell <oldmcdonnell@gmail.com>
2024-01-23 10:02:24 -0500

    Grayframe added

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ time.sleep(1) # wait for video to process
 
 while True:
     check, frame = video.read() # reads vide
-    cv2.imshow("My Video", frame) # sends data to visual file
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+    cv2.imshow("My Video", gray_frame_gau) # sends data to visual file
 
     key = cv2.waitKey(1) #keyboard key object
 
